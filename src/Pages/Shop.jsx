fb2d80048405f9bc1956fb1b937de02f28710e55
Aryan Yadav <aryanyd6497@gmail.com>
2025-07-26 13:01:40 +0530

    Update Shop.jsx

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,8 @@
 import React, { useRef } from 'react';
 import Hero from '../Components/Hero/Hero';
 import Popular from '../Components/Popular/Popular';
-import Offers from '../Components/Offers/Offers';
 import NewCollections from '../Components/NewCollections/NewCollections';
 import Newsletter from '../Components/NewsLetter/Newsletter';
-import Footer from '../Components/Footer/Footer';
 
 const Shop = () => {
   const trendingRef = useRef(null); // 👈 create ref for Trending section
